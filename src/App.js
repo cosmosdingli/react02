aa4775952a7e0d027f8ab3488dfd782a9c71ceb2
COSMOS-PC\cosmos <cosmosdingli@gmail.com>
2020-10-27 11:26:40 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React--xiaodingzi
+          Learn React--cosmosdingli@gmail.com
         </a>
       </header>
     </div>
